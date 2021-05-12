ef8aec6aec156db5798c34216c443aff57fcccb8
francis uloko <francisuloko@yahoo.com>
2021-05-12 23:40:24 +0100

    Fix rubocop offence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     # user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    
+
     if User.create(user_params)
       redirect_to new_user_path
     else
